cd6dcc7d67232b0af110d15e9b9a9118f43b774f
tonka7su <si@sipham.pw>
2021-10-31 19:22:57 +1100

    day 2 - white window

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -8,17 +8,133 @@
 
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine,
-        int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(HWND   hwnd,
+                                    UINT   uMsg,
+                                    WPARAM wParam,
+                                    LPARAM lParam) // WindowProc
 {
-    MessageBoxA(
-        0,
-        "This is Handmade Hero",
-        "Handmade Hero",
-        MB_OK | MB_ICONINFORMATION
-    );
+    LRESULT result = 0;
+
+    switch (uMsg)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            /*
+                typedef struct tagPAINTSTRUCT {
+                                HDC  hdc;
+                                BOOL fErase;
+                                RECT rcPaint;
+                                BOOL fRestore;
+                                BOOL fIncUpdate;
+                                BYTE rgbReserved[32];
+                                } PAINTSTRUCT, *PPAINTSTRUCT, *NPPAINTSTRUCT, *LPPAINTSTRUCT;
+            */
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(hwnd, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            PatBlt(
+                DeviceContext,
+                X,
+                Y,
+                Width,
+                Height,
+                WHITENESS
+                );
+            EndPaint(hwnd, &Paint);
+        } break;
+
+        default:
+        {
+            OutputDebugStringA("default\n");
+            result = DefWindowProcA(hwnd, uMsg, wParam, lParam);
+        } break;
+    }
+
+    return(result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE instance,
+        HINSTANCE prevInstance,
+        LPSTR cmdLine,
+        int cmdShow)
+{
+    // MessageBoxA(
+    //     0,
+    //     "This is Handmade Hero",
+    //     "Handmade Hero",
+    //     MB_OK | MB_ICONINFORMATION
+    // );
+
+    WNDCLASS WindowClass = {};
+
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = instance;
+    // WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(
+                                0,
+                                WindowClass.lpszClassName,
+                                "Handmade Hero",
+                                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                CW_USEDEFAULT,
+                                CW_USEDEFAULT,
+                                CW_USEDEFAULT,
+                                CW_USEDEFAULT,
+                                0,
+                                0,
+                                instance,
+                                0
+                                );
+        if (WindowHandle)
+        {
+            MSG Message;
+            for (;;)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }  
+    }
+    else 
+    {
+        // TODO: logging
+    }
+
     return(0);
 }
